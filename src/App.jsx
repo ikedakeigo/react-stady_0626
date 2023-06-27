46cc1ac9aac36a95b48ca2923defaf3147ac0554
silverskg <chokichoki_gm@i.softbank.jp>
2023-06-27 14:13:23 +0900

    react -

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+
+
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    if (num > 0){
+      if (num % 3 === 0) {
+        faceShowFlag  ||  setFaceShowFlag(true);
+      } else{
+        faceShowFlag &&  setFaceShowFlag(false);
+      };
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
+
+
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタん</button>
+      <button onClick={onClickCountUp}>カウント</button>
+      <br />
+
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag &&  <p>😃</p>}
     </>
   );
 };
